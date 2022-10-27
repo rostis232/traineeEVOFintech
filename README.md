6b9c2946e2f5cbea99475ca45a6b7004d74ccbd5
Rostislav Pylypiv <rostislav.pylypiv@gmail.com>
2022-10-27 10:48:58 +0300

    basic routes and handlers are added

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Project for Trainee EVO Fintech (second stage)
 
-Built in Go version 1.19.2
 
+- Built in Go version 1.19.2
+- Uses [the chi router](https://github.com/go-chi/chi/v5)
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"traineeEVOFintech/internal/config"
 )
 
 func main() {
+	fmt.Printf("Starting application on port %s\n", config.PortNumber)
+
 	srv := &http.Server{
-		Addr: config.PortNumber,
+		Addr:    config.PortNumber,
+		Handler: routes(),
 	}
-	
+
 	err := srv.ListenAndServe()
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatal(err)
 	}
 }
